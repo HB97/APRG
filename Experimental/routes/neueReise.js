a41327a13e9c46214565948fcb25f743e18020fe
SvenMaderer <Sven.Maderer@haw-hamburg.de>
2017-11-30 14:46:45 +0100

    no message

--- Experimental/routes/neueReise.js
+++ Experimental/routes/neueReise.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const reise = require('C:/Users/SvenM/OneDrive/Dokumente/GitShit/APRGlocal/client/reise');
+const db = require('./db');
+
 
 router.get('/', function(req, res, next){
-    console.log('Neu');
-    res.render('NeueReise',{
-        title: "Neue Reise"
-    });
+    if(req.session.authenticated){
+        res.render('NeueReise',{
+            title: "Neue Reise"
+        });
+    }
+    else res.redirect('/');
 
 });
 
 router.post('/name', (req, res)=>{
-    var neueReise = reise(req.body.neuerName);
-    console.log(neueReise);
-    res.redirect('/meineReisen');
+        let neueReiseName = req.body.neuerName;
+
+        db.sendQueryToDB('INSERT INTO Reisen(reise_name) VALUES (?)', neueReiseName, function(neueReise){
+            
+            console.log(neueReise.insertId);
+            
+            
+            db.sendQueryToDB('INSERT INTO benutzer_reisen(nutzer_id,reise_id) VALUES ( (SELECT nutzer_id from Benutzer WHERE nutzer_id = ?), (SELECT reise_id from Reisen WHERE reise_id = ?) )', [req.session.user.nutzer_id, neueReise.insertId], function(neuerEintrag){
+                console.log(neueReise);
+                console.log(neuerEintrag);
+                res.redirect('/meineReisen');
+
+            });
+
+            
+            
+            
+        });
+    
+        
+        
+        
 
 });
 
